5233f359f803cd49d093ab96cefa1e22d8d3198b
Elisabeth <elishu@stud.ntnu.no>
2026-03-12 13:26:31 +0100

    pucher|

---
+++ Kursdag 1/Oppgave-2/main.c
@@ -0,0 +1,17 @@
+/**
+ * @file main.c
+ * @author husev
+ * @date 2026-03-12
+ * @brief Main function
+ */
+#define F_CPU 4000000UL
+#include <avr/io.h>
+
+ 
+int main() {
+    //Definer en pin som ingang og en annen som utgang.
+    //Skrivn deretter en løkke som leser inngangen og setter utgangen høy dersom inngangen er høy og lav ellers.
+    
+    return 0;
+    
+}
