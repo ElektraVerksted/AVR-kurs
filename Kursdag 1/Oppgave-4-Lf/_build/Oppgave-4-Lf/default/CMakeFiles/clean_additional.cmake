5fd252531facbf11112b352426e87b322e3ba4f0
Torbjørn Gorsetbakk <Torbjorn.gorsetbakk@hotmail.com>
2026-03-16 14:03:14 +0100

    Opprettet oppgave 4 + LF

---
+++ Kursdag 1/Oppgave-4-Lf/_build/Oppgave-4-Lf/default/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,14 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "C:\\Users\\torbj\\Documents\\Linjeforening\\Elektra\\AVR-kurs\\Kursdag 1\\Oppgave-4-Lf\\out\\Oppgave-4-Lf\\default.cmf"
+  "C:\\Users\\torbj\\Documents\\Linjeforening\\Elektra\\AVR-kurs\\Kursdag 1\\Oppgave-4-Lf\\out\\Oppgave-4-Lf\\default.hex"
+  "C:\\Users\\torbj\\Documents\\Linjeforening\\Elektra\\AVR-kurs\\Kursdag 1\\Oppgave-4-Lf\\out\\Oppgave-4-Lf\\default.hxl"
+  "C:\\Users\\torbj\\Documents\\Linjeforening\\Elektra\\AVR-kurs\\Kursdag 1\\Oppgave-4-Lf\\out\\Oppgave-4-Lf\\default.mum"
+  "C:\\Users\\torbj\\Documents\\Linjeforening\\Elektra\\AVR-kurs\\Kursdag 1\\Oppgave-4-Lf\\out\\Oppgave-4-Lf\\default.o"
+  "C:\\Users\\torbj\\Documents\\Linjeforening\\Elektra\\AVR-kurs\\Kursdag 1\\Oppgave-4-Lf\\out\\Oppgave-4-Lf\\default.sdb"
+  "C:\\Users\\torbj\\Documents\\Linjeforening\\Elektra\\AVR-kurs\\Kursdag 1\\Oppgave-4-Lf\\out\\Oppgave-4-Lf\\default.sym"
+  )
+endif()
